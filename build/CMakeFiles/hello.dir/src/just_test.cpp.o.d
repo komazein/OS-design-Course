ca0c0f0639418d0bc1680462ab973a02678ac2a0
1554823142 <1554823142@qq.com>
2025-05-22 15:00:42 +0800

    test

---
+++ build/CMakeFiles/hello.dir/src/just_test.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/hello.dir/src/just_test.cpp.o: \
+ /home/kaku/Documents/OS_ZXY/src/just_test.cpp /usr/include/stdc-predef.h
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "OS start!" << std::endl;
+}
