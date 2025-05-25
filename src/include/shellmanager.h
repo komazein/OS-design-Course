0ca3f9ede93ae2965029a45e2e758b35adb48da7
komazein <1871329267@qq.com>
2025-05-25 23:50:47 +0800

    feats: shell framework and flex parser

---
+++ src/include/shellmanager.h
@@ -0,0 +1,48 @@
+
+#include "fs.h";
+
+class shell_manager{
+
+    private:
+        fs* file_system_;  // 文件系统指针
+        dentry* current_dir_;  // 当前目录项
+        
+    
+    public:
+        shell_manager(fs* file_system) :file_system_(file_system), current_dir_(nullptr) {
+            // 初始化当前目录为根目录
+            current_dir_ = file_system_->get_root_dentry();
+        }
+
+        // execute shell command for dir only
+        void command_mkdir(const std::string& dirname);
+
+        void command_cd(const std::string& dirname);
+
+        void command_ls();
+
+        void command_rm(const std::string& dirname);
+
+        void command_lkdir(const std::string& dirname);
+
+        void command_lndir(const std::string& dirname);
+
+        void command_find(const std::string& filename);
+
+        // execute shell command for file only
+        void command_touch(const std::string& filename);
+
+        void command_delete(const std::string& filename);
+
+        void command_cat(const std::string& filename);
+
+        // shell commmand for user
+        void exit();
+
+        void format();
+
+        void help();
+
+
+        
+};
