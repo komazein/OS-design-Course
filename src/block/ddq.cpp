2ace6f930d3bc4a880a283dd9d1a7349553f7994
1554823142 <1554823142@qq.com>
2025-05-27 14:48:32 +0800

    merge wgl

--- src/block/ddq.cpp
+++ src/block/ddq.cpp
@@ -642,6 +642,9 @@ bool blockScheduler::writeSIMfromBLOCK(inode&id,char*a)
     //     cout<<all[i]<<" ";
     // cout<<endl;
     writeBlocknumFORsim(all,strlen(a),id,a);
+    for(int i=0;i<all.size();i++)
+        cout<<i<<" "<<all[i]<<endl;
+    writeBlocknumFORsim(all,strlen(a),id,a);
     return true;
 }
 void blockScheduler::SIMwriteBK(vector<size_t>newlist,size_t n,char*a)
@@ -672,7 +675,7 @@ void blockScheduler::SIMwriteBK(vector<size_t>newlist,size_t n,char*a)
     }
     fclose(fp);
 }
-void blockScheduler::writeBlocknumFORsim(vector<size_t>&all,size_t n,inode&id,char*a)
+void blockScheduler::writeBlocknumFORsim(vector<size_t>&all,size_t n,inode&id,char*a,char*a)
 {
     size_t num=(id.i_size+511)/512;
     vector<size_t>newlist;
@@ -730,8 +733,17 @@ void blockScheduler::writeBlocknumFORsim(vector<size_t>&all,size_t n,inode&id,ch
     }
     id.i_block[ABLE_DIRECT_SIM-ABLE_MULTI_SIM]=all[all.size()-1];
     all.erase(all.end()-1);
-    simwriteTree(id.i_block[ABLE_DIRECT_SIM-ABLE_MULTI_SIM],all,num,newlist);
-    SIMwriteBK(newlist,id.i_size,a);
+    simwriteTree(id.i_block[ABLE_DIRECT_SIM-ABLE_MULTI_SIM],all,MAXnumInBlock,newlist);
+    size_t now_byte=n;
+    FILE *fp=fopen("../disk.img","r+");
+    for(int i=0;i<newlist.size();i++)
+        
+    for(int i=0;i<newlist.size();i++)
+    {
+        fseek(fp,sizeof(super_block)+INODENUM*sizeof(inode)+newlist[i]*512,SEEK_SET);
+        fwrite(a,sizeof(char),max(now_byte,(size_t)512),fp);
+        now_byte-=max(now_byte,(size_t)512);
+    }
 }
 void blockScheduler::simwriteTree(size_t block_id,vector<size_t>&all,size_t n,vector<size_t>&newlist)
 {
@@ -820,18 +832,4 @@ void blockScheduler::simwriteTree(size_t block_id,vector<size_t>&all,size_t n,ve
         simwriteTree(tree.front().first,all,tree.front().second,newlist);
         tree.pop();
     }
-}
-
-void blockScheduler::new_disk()
-{
-
-    sb->newdisk();
-}
-inode*blockScheduler::iget(bool ifroot)
-{
-    return sb->iget(ifroot);
-}
-size_t blockScheduler::getfreeblocknum()
-{
-    return sb->getfreeBlocknum();
 }
