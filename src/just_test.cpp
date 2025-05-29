1057e09186df331749f3803d95cf65b8752c97f7
1554823142 <1554823142@qq.com>
2025-05-29 08:53:51 +0800

    temp

--- src/just_test.cpp
+++ src/just_test.cpp
@@ -51,27 +51,8 @@ void TESTWRITEBACK()
         fs.command_mkdir(test_name);
     }
     fs.Exit();
-    
 }
-void new_disk()
-{
 
-    dirTree* dir_tree_;     
-    dentry* current_dir_;  
-    // file_manager * file_manager_;
-    blockScheduler* bs_; 
-
-
-    dir_tree_ = new dirTree;
-
-    bs_ = new blockScheduler(dir_tree_);
-
-    dir_tree_->set_bs(bs_);
-
-    bs_->new_disk();
-    bs_->writesuperblock();
-    bs_->ReWrinode(*dir_tree_->get_root()->get_inode(),false);
-}
 
 /*
 void func()
